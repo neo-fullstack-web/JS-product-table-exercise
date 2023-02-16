7fbc93e11238bc01d6ec841aa9ea1bacb21ed32a
Martin Rodriguez <neomartinr@gmail.com>
2023-02-16 19:21:52 -0300

    Basic table products

---
+++ js/object-exercise.js
@@ -0,0 +1,99 @@
+const Products = [
+    {
+        name: 'PS5',
+        description: 'La consola más potente de Sony al día de la fecha',
+        price: 290000,
+        stock: true,
+        image: 'https://as.com/meristation/imagenes/2020/11/06/reportajes/1604654372_894608_1604656126_noticia_normal.jpg',
+        games: ['God of War Origin']
+    },
+    {
+        name: 'PS4',
+        description: 'La consola anterior de Sony',
+        price: 140000,
+        stock: true,
+        image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg'
+    },
+    {
+        name: 'PS3',
+        description: 'La consola anterior de Sony',
+        price: 70000,
+        stock: false,
+        image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg',
+        jostick: true,
+        games: ['Uncharted 3']
+    },
+
+    {
+        name: 'PS2',
+        description: 'La consola anterior de Sony',
+        price: 30000,
+        stock: false,
+    },
+    {
+        name: 'XBOX Series X',
+        description: 'La consola más potente de Microsoft al día de la fecha',
+        price: 279000,
+        stock: true,
+        image: 'https://www.atajo.com.ar/images/0000000RRT-0000234357RRT-00002-Consola-Xbox-Series-X-01.jpg',
+        games: ['Halo Infinite'],
+        jostick: true,
+
+    },
+    {
+        name: 'XBOX One',
+        description: 'La consola anterior de Microsoft',
+        price: 115000,
+        stock: false,
+        image: 'https://i.blogs.es/a7dc9c/fc7174d71089999f6a7e15c1d16/1366_2000.png'
+    },
+    {
+        name: 'XBOX 360',
+        description: 'La consola de Microsoft que compite con la PS3',
+        price: 60000,
+        stock: true,
+        image: 'https://http2.mlstatic.com/D_NQ_NP_686099-MLA32731207921_112019-O.webp',
+    }
+];
+
+//1- Obtener el body de la tabla para poder modificarlo desde JS
+const tableBody = document.querySelector('#table-body');
+
+
+//2- Definir una función para iterar el array
+function renderizarTabla() {
+    tableBody.innerHTML = '';
+    //3- Iterar el array para acceder a cada producto
+    Products.forEach((producto) => {
+        // let imageSrc = '/assets/images/no-product.png';
+
+        // if(producto.image) {
+        //     imageSrc = producto.image;
+        // }
+
+        let imageSrc = producto.image ? producto.image : '/assets/images/no-product.png';
+        //4- Introducir dentro del tbody una fila por producto con sus respectivas celdas
+        const tableRow = `<tr>
+                                <td><img src="${imageSrc}" width="120" alt="${producto.name}"></td>
+                                <td>${producto.name}</td>
+                                <td>${producto.description}</td>
+                                <td>$ ${producto.price}</td>
+                                <td>📦🎮</td>
+                                <td>🟥✏️⭐</td>
+                            </tr>`
+        tableBody.innerHTML += tableRow;
+    });
+
+}
+
+renderizarTabla();
+
+
+// product
+//     name
+//     description
+//     price
+//     imagen
+//     stock?
+//     jostick?
+//     games?
